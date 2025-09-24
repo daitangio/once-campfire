4bbca29e283d69488d3922d196f3615d6faa2302
Giovanni Giorgi <daitangio@users.noreply.github.com>
2025-09-24 14:24:03 +0000

    Added extra test on banned users

--- test/controllers/accounts_controller_ban_test.rb
+++ test/controllers/accounts_controller_ban_test.rb
@@ -1,17 +1,29 @@
 require "test_helper"
 
-class BanEditingTest < ActionDispatch::IntegrationTest 
+class BanEditingTest < ActionDispatch::IntegrationTest
   setup do
     sign_in :banner
   end
 
-  test "edit" do
+  test "banned_user_get_forbidden_access" do
     assert users(:banner).member?
     assert users(:banner).banned?
 
     get edit_account_url
     # if you are banned, you cannot get there
     assert_response :forbidden
-    
   end
-end    
+
+  # Generated with ChatGPT
+  test "admin can unban banner user" do
+    sign_in :david
+
+    banner = users(:banner)
+    assert banner.banned?, "Banner should start banned for this test"
+
+    patch account_user_path(banner), params: { user: { role: "member", banned: "0" } }
+
+    assert_redirected_to edit_account_url
+    assert_not banner.reload.banned?, "Banner should be unbanned after admin update"
+  end
+end
