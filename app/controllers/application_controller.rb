18c1519a542c67ddcefbe8a3fb76f4dfefbfb962
Giovanni Giorgi <daitangio@users.noreply.github.com>
2025-09-30 10:42:05 +0000

    Added VERY BASIC Internazionalization support and translation. Not yet complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include AllowBrowser, Authentication, Authorization, SetCurrentRequest, SetPlatform, TrackedRoomVisit, VersionHeaders
   include Turbo::Streams::Broadcasts, Turbo::Streams::StreamName
+
+  # GG Added i18 support here with locale param
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
